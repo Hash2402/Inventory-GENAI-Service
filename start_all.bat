a2fce6568f5edb369fb8a8a96ed73e2f3096616f
Harsh Sharma <71495171+Hash2402@users.noreply.github.com>
2025-07-10 20:59:49 -0700

    Update start_all.bat

--- start_all.bat
+++ start_all.bat
@@ -1,4 +1,4 @@
 start cmd /k "cd inventory-service && uvicorn main:app --port 8000 --reload"
 start cmd /k "cd mcp-server && uvicorn main:app --port 8001 --reload"
 timeout /t 2
-start cmd /k "cd mcp_client && python cli.py"
+start cmd /k "cd mcp-client && python cli.py"
